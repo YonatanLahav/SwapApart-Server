9c5fb26ed35af073a96f4b8a529c6686c9fc37e3
roy maman <roey.maman@gmail.com>
2023-04-13 16:53:23 +0300

    a

--- routes/api/users.js
+++ routes/api/users.js
@@ -11,8 +11,8 @@ const User = require('../../models/User');
 // @access   Private
 router.get('/', auth, async (req, res) => {
     try {
-        const users = await User.find().select('-password');
-        res.json(users);
+        const user = await User.findById(req.user.id).select('-password');
+        res.json(user);
     } catch (err) {
         console.error(err.message);
         res.status(500).send('Server error');
