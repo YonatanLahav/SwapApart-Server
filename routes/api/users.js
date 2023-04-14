c18360acc1dee775427b697b40fb5e752690d6b4
YonatanLahav <Jonatan.Lahav@gmail.com>
2023-04-14 14:19:22 +0300

    a

--- routes/api/users.js
+++ routes/api/users.js
@@ -3,16 +3,15 @@
 const express = require('express');
 const router = express.Router();
 const auth = require('../../middleware/auth');
-
 const User = require('../../models/User');
 
 // @route    GET api/users
-// @desc     Get all users
+// @desc     Get user.
 // @access   Private
 router.get('/', auth, async (req, res) => {
     try {
-        const users = await User.find().select('-password');
-        res.json(users);
+        const user = await User.findById(req.user.id).select('-password');
+        res.json(user);
     } catch (err) {
         console.error(err.message);
         res.status(500).send('Server error');
@@ -21,8 +20,8 @@ router.get('/', auth, async (req, res) => {
 
 // @route    GET api/users/:id
 // @desc     Get user by ID
-// @access   Public
-router.get('/:id', async (req, res) => {
+// @access   Private
+router.get('/:id', auth, async (req, res) => {
     try {
         const user = await User.findById(req.user.id).select('-password');
 
@@ -37,6 +36,45 @@ router.get('/:id', async (req, res) => {
             return res.status(404).json({ msg: 'User not found' });
         }
         res.status(500).send('Server error');
+    }
+});
+
+// @route   PUT api/users
+// @desc    Update user.
+// @access  Private
+router.put('/:id', auth, async (req, res) => {
+    const { startDate, endDate, country, region, city, minRoomsNum, minBathroomsNum } = req.body;
+
+    // Build plan object
+    const planFields = {};
+    if (startDate) planFields.startDate = startDate;
+    if (endDate) planFields.endDate = endDate;
+    if (country) planFields.country = country;
+    if (region) planFields.region = region;
+    if (city) planFields.city = city;
+    if (minRoomsNum) planFields.minRoomsNum = minRoomsNum;
+    if (minBathroomsNum) planFields.minBathroomsNum = minBathroomsNum;
+
+    try {
+        let plan = await Plan.findById(req.params.id);
+
+        if (!plan) return res.status(404).json({ msg: 'Plan not found' });
+
+        // Make sure user owns plan
+        if (plan.userId.toString() !== req.user.id) {
+            return res.status(401).json({ msg: 'Not authorized' });
+        }
+
+        plan = await Plan.findByIdAndUpdate(
+            req.params.id,
+            { $set: planFields },
+            { new: true }
+        );
+
+        res.json(plan);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
     }
 });
 
