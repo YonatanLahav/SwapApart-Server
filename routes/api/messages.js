53716340ce671b57d569d2bb75b3f6f0c77dccbc
YonatanLahav <Jonatan.Lahav@gmail.com>
2023-04-20 12:08:24 +0300

    add basic messages api

---
+++ routes/api/messages.js
@@ -0,0 +1,43 @@
+// Import required modules
+const express = require('express');
+const router = express.Router();
+const Message = require('../../models/Message');
+const Match = require('../../models/Match');
+const auth = require('../../middleware/auth');
+
+router.get('/', auth, async (req, res) => {
+    console.log("A");
+});
+
+// Create a new message
+router.post('/', auth, async (req, res) => {
+    const { match, sender, text } = req.body;
+    try {
+        const newMessage = new Message({ match, sender, text });
+        const message = await newMessage.save();
+
+        // Update the match's last message and last update time
+        const updatedMatch = await Match.findByIdAndUpdate(
+            req.body.match,
+            {
+                $set: {
+                    lastMessage: message._id,
+                    lastUpdate: Date.now(),
+                },
+                $push: { messages: { $each: [message._id], $position: 0 } },
+            },
+            { new: true }
+        );
+
+        if (!updatedMatch) {
+            return res.status(404).json({ msg: 'Match not found' });
+        }
+
+        res.json(message);
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send('Server Error');
+    }
+});
+
+module.exports = router;
