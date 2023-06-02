2a5a61023b34e252e2e91bf63fb0c5c6bb5e56e1
YonatanLahav <Jonatan.Lahav@gmail.com>
2023-06-02 16:06:51 +0300

    cleanup the code and added comments

--- server.js
+++ server.js
@@ -1,86 +1,99 @@
 // /server.js
-const express = require('express');
-const connectDB = require('./config/db');
-const cors = require('cors');
-const auth = require('./middleware/auth'); // import auth middleware
-const bodyParser = require('body-parser');
-const http = require('http');
-const jwt = require('jsonwebtoken');
-const config = require('config');
-const socket = require('socket.io');
-const { getUserId, getToUserId } = require('./middleware/socket');
-const User = require('./models/User');
-const app = express();
-
-// Create http server
-const server = http.createServer(app);
 
 /**
- * Socket.io
+ * Required modules
  */
-// Create the socket
-const io = socket(server, {
+const express = require('express'); // Express.js framework for building web applications
+const http = require('http'); // Node.js HTTP module for creating server
+const socket = require('socket.io'); // Socket.IO for real-time communication
+const cors = require('cors'); // Cross-Origin Resource Sharing middleware
+const bodyParser = require('body-parser'); // Body parsing middleware
+const config = require('config'); // Configuration module
+
+const connectDB = require('./config/db'); // Custom module to connect to MongoDB database
+const auth = require('./middleware/auth'); // Custom authentication middleware
+const { getUserId, getToUserId } = require('./middleware/socket'); // Custom middleware functions
+const User = require('./models/User'); // User model
+
+const app = express(); // Create an Express application
+const server = http.createServer(app); // Create an HTTP server using Express app
+const io = socket(server, { // Create Socket.IO instance with server
     cors: {
-        origin: "http://localhost:3000",
-        credentials: true,
+        origin: "http://localhost:3000", // Set the allowed origin for CORS
+        credentials: true, // Enable CORS credentials
     },
 });
 
-var onlineUsers = new Map();
+const onlineUsers = new Map(); // Create a Map to store online users
 
+/**
+ * Socket.IO Configuration
+ */
 io.on("connection", (socket) => {
-    console.log("new connection");
-    // Event listener that add a new user to onlineUser map.
+    console.log("New connection");
+
+    // Event listener that adds a new user to the onlineUsers map
     socket.on("add_user", (token) => {
         try {
             const userId = getUserId(token);
             onlineUsers.set(userId, socket.id);
             console.log(onlineUsers);
+        } catch (err) {
+            console.log(err);
         }
-        catch (err) { console.log(err); }
     });
-    // Event listener that handle a new message.
-    // msg = { match, sender, text}
+
+    // Event listener that handles a new message
     socket.on("send_msg", async (msg) => {
         try {
             const toUserId = await getToUserId(msg.match, msg.sender);
             const sendToUserSocket = onlineUsers.get(toUserId);
-            console.log(toUserId)
+            console.log(toUserId);
             if (sendToUserSocket) {
                 socket.to(sendToUserSocket).emit("msg_recieve", msg);
             }
             console.log(msg);
-        } catch (err) { console.log(err); }
+        } catch (err) {
+            console.log(err);
+        }
     });
 });
+
 /**
- * 
+ * Middleware Setup
  */
-
-// Enable Cross-Origin Resource Sharing
-app.use(cors());
-
-// Set a higher payload limit
-app.use(bodyParser.json({ limit: '50mb' }));
+app.use(cors()); // Enable Cross-Origin Resource Sharing
+app.use(bodyParser.json({ limit: '50mb' })); // Set a higher payload limit for parsing request bodies
 app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
-// Connect to MongoDB database
-connectDB();
 
-// Init Middleware
+/**
+ * Database Connection
+ */
+connectDB(); // Connect to MongoDB database
+
+/**
+ * Initialize Middleware
+ */
 app.use(express.json({ extended: false }));
 
-// Define Routes
-app.use('/login', require('./routes/auth/login'));
-app.use('/register', require('./routes/auth/register'));
-app.use('/api/users', auth, require('./routes/api/users'));
-app.use('/api/plans', auth, require('./routes/api/plans'));
-app.use('/api/matches', auth, require('./routes/api/matches'));
-app.use('/api/swipes', auth, require('./routes/api/swipes'));
-app.use('/api/messages', auth, require('./routes/api/messages'));
+/**
+ * Route Configuration
+ */
+app.use('/login', require('./routes/auth/login')); // Login route
+app.use('/register', require('./routes/auth/register')); // Register route
+app.use('/api/users', auth, require('./routes/api/users')); // Users API route
+app.use('/api/plans', auth, require('./routes/api/plans')); // Plans API route
+app.use('/api/matches', auth, require('./routes/api/matches')); // Matches API route
+app.use('/api/swipes', auth, require('./routes/api/swipes')); // Swipes API route
+app.use('/api/messages', auth, require('./routes/api/messages')); // Messages API route
 
-// Root endpoint
+/**
+ * Root Endpoint
+ */
 app.get('/', (req, res) => res.send('Hello World!'));
 
-// Start the server
+/**
+ * Start the Server
+ */
 const port = process.env.PORT || 5000;
 server.listen(port, () => console.log(`Server running on port ${port}`));
