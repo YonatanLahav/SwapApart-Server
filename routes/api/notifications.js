a8c172b8a5378a61a7e2c5229bb8654dad5ec235
YonatanLahav <Jonatan.Lahav@gmail.com>
2023-06-07 15:37:11 +0300

    set up delete notification api

--- routes/api/notifications.js
+++ routes/api/notifications.js
@@ -1,11 +1,7 @@
-// /routes/api/notifications.js
-
 const express = require('express');
 const router = express.Router();
 const auth = require('../../middleware/auth');
-const Plan = require('../../models/Plan');
 const User = require('../../models/User');
-const Match = require('../../models/Match');
 
 /**
  * @route   GET api/notifications
@@ -13,10 +9,25 @@ const Match = require('../../models/Match');
  * @access  Private
  */
 router.get('/', auth, async (req, res) => {
-    // Find the user by ID
-    const user = await User.findById(req.user.id);
     try {
+        const user = await User.findById(req.user.id);
         res.json(user.notifications);
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+});
+
+/**
+ * @route   DELETE api/notifications
+ * @desc    Delete all notifications of the user.
+ * @access  Private
+ */
+router.delete('/', auth, async (req, res) => {
+    try {
+        const user = await User.findById(req.user.id);
+        user.notifications = []; // Remove all notifications
+        await user.save();
+        res.json({ message: 'Notifications deleted successfully' });
     } catch (err) {
         res.status(500).json({ message: err.message });
     }
